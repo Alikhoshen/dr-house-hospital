4a6d515d918ab7f185fc4f70f06d6f018fa7f46c
Ali KHOSHEN <ali.khoshen3@gmail.com>
2025-03-01 00:02:24 +0100

    adding the set up of spring project

---
+++ drhouse-hospital/src/main/java/com/drhousehospital/management/DrhouseHospitalApplication.java
@@ -0,0 +1,13 @@
+package com.drhousehospital.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrhouseHospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrhouseHospitalApplication.class, args);
+	}
+
+}
---
+++ drhouse-hospital/src/test/java/com/drhousehospital/management/DrhouseHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drhousehospital.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrhouseHospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
